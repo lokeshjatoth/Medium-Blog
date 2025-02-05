6cda721c41be24b45383ee26e5ea790a619d25b9
lokeshjatoth <lokeshjatothit@gmail.com>
2025-02-05 13:29:23 +0530

    Made Authentication changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Signup from './pages/Signup'
 import Signin from './pages/Signin'
@@ -7,17 +7,31 @@ import Blog from './pages/Blog'
 import Blogs from './pages/Blogs'
 import { Publish } from './pages/Publish'
 
+const isAuthenticated = () => {
+  return !!localStorage.getItem('token'); // Modify based on your auth logic
+};
+
 function App() {
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/signin" element={<Signin/>}/>
-          <Route path="/blog/:id" element={<Blog/>}/>
-          <Route path='blogs' element={<Blogs/>}/>
-          <Route path='/publish' element={<Publish/>}/>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route
+            path="/blog/:id"
+            element={isAuthenticated() ? <Blog /> : <Navigate to="/signup" />}
+          />
+          <Route
+            path="/blogs"
+            element={isAuthenticated() ? <Blogs /> : <Navigate to="/signup" />}
+          />
+          <Route
+            path="/publish"
+            element={isAuthenticated() ? <Publish /> : <Navigate to="/signup" />}
+          />
+          <Route path="*" element={<Navigate to="/signup" />} /> {/* Default route */}
         </Routes>
       </BrowserRouter>
         
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -5,41 +5,45 @@ import { useBlogs } from "../hooks";
 
 
 const Blogs = () => {
-    const {loading, blogs} = useBlogs();
-    console.log(blogs)
-
+    const { loading, blogs } = useBlogs();
+    console.log(blogs);
 
     if (loading) {
-        return <div>
-            <Appbar /> 
-            <div  className="flex justify-center">
-                <div className="flex flex-col">
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-                    <BlogSkeleton />
-                    <BlogSkeleton />
+        return (
+            <div>
+                <Appbar />
+                <div className="flex justify-center">
+                    <div className="flex flex-col">
+                        <BlogSkeleton />
+                        <BlogSkeleton />
+                        <BlogSkeleton />
+                        <BlogSkeleton />
+                        <BlogSkeleton />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <Appbar />
+            <div className="flex justify-center">
+                <div className="max-w-xl">
+                    {blogs.slice().reverse().map(blog => (
+                        <BlogCard
+                            key={blog.id} // Always add a key when mapping
+                            authorName={blog.author.name || "Anonymous"}
+                            title={blog.title}
+                            content={blog.content}
+                            id={blog.id}
+                            publishedDate={"4th Feb 2025"}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
-    }
-
-  return (
-    <div >
-        <Appbar/>
-        <div className="flex justify-center">
-        <div className="max-w-xl">
-            {blogs.map( blog => <BlogCard
-                authorName={blog.author.name || "Anonymous"}
-                title={blog.title}
-                content={blog.content}
-                id={blog.id}
-                publishedDate={"4th Feb 2025"}
-            />)}
-        </div>
-        </div>
-    </div>
-  )
+    );
 }
 
-export default Blogs
+export default Blogs;
